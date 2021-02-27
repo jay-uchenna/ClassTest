ac1ad4bfee831510638ca28e9221b4f46f3448a0
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2021-02-27 23:43:47 +0300

    learning to write classes in new files

--- ClassTest/main.cpp
+++ ClassTest/main.cpp
@@ -6,67 +6,19 @@
 //
 
 #include <iostream>
+#include "dosage_check.hpp"
 
-class just_a_test{
-    private:
-        char name_of_drug;
-        int total_doze;
-        double doze_per_dar;
-    public:
-        just_a_test(){
-            name_of_drug = 'e';
-            total_doze = 100;
-            doze_per_dar = 2.5;
-        }
-        just_a_test(char d){
-            name_of_drug = d;
-            total_doze = 100;
-            doze_per_dar = 2.5;
-        }
-        just_a_test(char d, int e){
-           name_of_drug = d;
-           total_doze = e;
-           doze_per_dar = 2.5;
-        }
-        just_a_test(double f){
-            name_of_drug = 'a';
-            total_doze = 40;
-            doze_per_dar = f;
-        }
-        just_a_test(int a){
-            name_of_drug = 'v';
-            total_doze = a;
-            doze_per_dar = 3.5;
-        }
-        just_a_test(char d, int e, double f);
-        void no_remaining(){
-            total_doze -= doze_per_dar;
-        };
-        char show_drug() const;
-        int get_remaining() const;
-    
-};
- inline just_a_test::just_a_test(char d, int e, double f){
-    name_of_drug = d;
-    total_doze = e;
-    doze_per_dar = f;
-}
-inline char just_a_test::show_drug() const{
-    return name_of_drug;
-}
-inline int just_a_test::get_remaining() const{
-    return this ->total_doze;
-}
 
-void display(const just_a_test* );
+
+void display(const dosage_check* );
 
 int main() {
     
-    just_a_test person1, person2('h'), person3('f', 20), person4('q', 200, 5.8), person5(92), person6(92.5);
+    dosage_check person1, person2('h'), person3('f', 20), person4('q', 200, 5.8), person5(92), person6(92.5);
     
     display(&person1);
     
-    for (int i=0; i < 10; i++){
+    for (int i=0; i < 9; i++){
     person1.no_remaining();
     person2.no_remaining();
     person3.no_remaining();
@@ -80,7 +32,7 @@ int main() {
     return 0;
 }
 
-void display(const just_a_test* a){
+void display(const dosage_check* a){
     for (int i = 0; i < 6; i++)
     std::cout <<(a-i)->show_drug() << " remains " << (a-i)->get_remaining() << " dozes\n";
 }
